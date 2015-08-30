f5bb41f1eb8b8e67c0150954e6d30eec39916ca8
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2015-08-30 23:17:33 +0900

    Add spec for Ajimi::Config

---
+++ spec/ajimi/config_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "Ajimi#Config" do
+
+  describe ".load" do
+    let(:ajimi_file_content) { <<-"AJIMIFILE"
+source_host "source_host_value"
+source_user "source_user_value"
+source_key "source_key_value"
+target_host "target_host_value"
+target_user "target_user_value"
+target_key "target_key_value"
+check_root_path "check_root_path_value"
+    AJIMIFILE
+    }
+
+    let(:loaded_config) { {
+      source_host: "source_host_value",
+      source_user: "source_user_value",
+      source_key: "source_key_value",
+      target_host: "target_host_value",
+      target_user: "target_user_value",
+      target_key: "target_key_value",
+      check_root_path: "check_root_path_value"
+    } }
+
+    before do
+      expect(::File).to receive(:read).and_return(ajimi_file_content)
+      @config = Ajimi::Config.load("./Ajimifile")
+    end
+
+    it "returns config hash" do
+      expect(@config).to eq loaded_config
+    end
+    
+  end
+end
