1f6fd8f8655e25104e6c7c90e7c17d10c2b9c329
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2015-09-21 11:35:08 +0900

    Refactor CONFIG_KEYWORDS

--- lib/ajimi/config.rb
+++ lib/ajimi/config.rb
@@ -17,19 +17,19 @@ module Ajimi
       instance_eval(File.read(path), path) if path
     end
 
-    CONFIG_KEYWORDS = [
-      :source_host,
-      :source_user,
-      :source_key,
-      :target_host,
-      :target_user,
-      :target_key,
-      :check_root_path,
-      :ignore_paths,
-      :ignore_contents,
-      :pending_paths,
-      :pending_contents
-    ]
+    CONFIG_KEYWORDS = %i(
+      source_host
+      source_user
+      source_key
+      target_host
+      target_user
+      target_key
+      check_root_path
+      ignore_paths
+      ignore_contents
+      pending_paths
+      pending_contents
+    )
 
     CONFIG_KEYWORDS.each do |keyword|
       define_method(keyword) { |param| @config[keyword] = param }
